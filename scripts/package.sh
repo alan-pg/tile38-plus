88d7a1589a0058b5a7737f6e2e4aa4f2fdbe4fc1
alan goncalves <alan92goncalves@gmail.com>
2026-02-27 22:40:51 -0300

    build: fallback to default version when git describe fails

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,7 +6,7 @@ cd $(dirname "${BASH_SOURCE[0]}")/..
 PLATFORM="$1"
 GOOS="$2"
 GOARCH="$3"
-VERSION=$(git describe --tags --abbrev=0)
+VERSION=$(git describe --tags --abbrev=0 2>/dev/null || echo "1.0.0-custom")
 
 echo Packaging $PLATFORM Binary
 
